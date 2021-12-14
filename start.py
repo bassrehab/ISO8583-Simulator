66277ed8ef1bf47a2195baa29a9ddb66e71560b3
Subhadip Mitra <3048879+bassrehab@users.noreply.github.com>
2021-12-14 23:19:17 +0800

    Update start.py

--- start.py
+++ start.py
@@ -1,9 +1,9 @@
 if __name__ == "__main__":
     print('*********************************************************')
-    print('*        IS) 8583 Simulator                              *')
+    print('*        ISO 8583 Simulator                              *')
     print('*********************************************************\n')
 
 
-    # Start UTU-Sim server.
+    # Start Sim server.
     from classes.server.server import SimulatorServer
     SimulatorServer()
